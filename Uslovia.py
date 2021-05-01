45411472c96aed4672e66cd0adad22f8df13e001
Azim <urmanbetovazim@gmail.com>
2021-05-01 23:13:50 +0600

    first week

---
+++ Uslovia.py
@@ -0,0 +1,131 @@
+# problem_2
+a = 2**3
+b = 3**2
+if a > b:
+    print("a больше")
+else:
+    print("b больше")
+
+# problem_22
+# a = int(input("Enter numb a:"))
+# b = int(input("Enter numb b:"))
+# c = int(input("Enter numb c:"))
+
+# if a < b and a < c:
+#     print("Самое маленькое число а!")
+#     if b > c:
+#         print("Самое большое число b!")
+#     else:
+#         print("Самое большое число c!")
+# elif b < a and b < c:
+#     print("Самое маленькое число b!")
+#     if a > c:
+#         print("Самое большое число a!")
+#     else:
+#         print("Самое большое число c!")
+# elif c < a and c < b:
+#     print("Самое маленькое число c!")
+#     if b > a:
+#         print("Самое большое число b!")
+#     else:
+#         print("Самое большое число a!")
+
+# problem_15
+a = 17391
+b = 546
+c = 934
+
+if a % 17 < b % 17 and a % 17 < c % 17:
+    print("Наименьший остаток а!")
+elif b % 17 < a % 17 and b % 17 < c % 17:
+    print("Наименьший остаток b!")
+else:
+    print("Наименьший остаток c!")
+
+# problem_39
+x = 13
+if x * x > 172:
+    print("f")
+elif x * x * x > 172:
+    print("a")
+
+
+
+
+# problem_5
+a = int(input("Enter numb: "))
+if a % 2 == 0:
+    print("Четное число!")
+else:
+    print("Нечетное число!")
+if a % 3 == 0:
+    print("Нечетное число!")
+else:
+    print("Четное число!")
+if a ** 2 > 1000:
+    print("а больше 1000!")
+else:
+    print("а Меньше 1000!")
+
+# problem_3
+a = int(input("Enter numb: "))
+if a >= 0 and a <= 21:
+    print("Число в разрешенном диапазоне!")
+elif a >= 57 and a <= 100:
+    print("Число в разрешенном диапазоне!")
+else:
+    print("Число в неразрешенном диапазоне!")
+
+# problem_7
+a = int(input("Введите любое число:"))
+if a > 0 or a < 0 or a == 0:
+    print(":-)")
+
+# problem_9
+name = "azim"
+age = 18
+password = 5555
+if name == "azim":
+    if age == 18:
+        if password == 5555:
+            print("Right")
+
+# problem_45
+a = 10//5 
+b = 10/5
+c = a + b
+if a == b:
+    print(c)
+else:
+    print("Не равны")
+
+# problem_33
+a = 123
+if a > 0 or a == 0:
+    print(a)
+else:
+    print(a-a-a)
+
+
+
+# problem_11
+
+a = 10
+b = 5
+if a > 0 and b > 0:
+    print("Положительное число")
+else:
+    print("Не положительное число")
+
+
+# problem_0
+a = 10
+b = 5
+if a > 0 and b > 0:
+    print("положительные")
+
+
+
+    
+
+
---
+++ dannye.py
@@ -0,0 +1,77 @@
+# problem_4
+a = (17 * 3) < (12 * 5)
+print(a)
+b = (12 * 3) <(13 * 7)
+print(b)
+c = (4 ** 5) > (512 + 512)
+print(c)
+
+# problem_1
+a = 17925
+b = (34 ** 2) >= a
+print(b)
+c = (26 * 3) >= a
+print(c)
+d = (17 * 33) >= a
+print(d)
+e = (4394 * 4) >= a 
+print(e)
+
+# problem_3
+a = 9
+b = 5
+c = a + b
+print(c)
+d = c * 5
+print(d)
+e = d ** 2
+print(e)
+f = e - 193432
+print(f)
+
+# problem_2
+# 1
+a = 5
+b = 4
+c = 6
+i = c > a > b
+print(i)
+# 2
+a = 7
+b = 3
+c = a % b
+f = c * 4.8
+print(f)
+# 3
+a = 5 + 3
+b = 11 - 3
+c = a == b
+print(c)
+# 4
+a = 5 + 2
+b = 9 - 1
+c = a != b
+print(c)
+# problem_5
+a = -21 // 10
+print(a,"отриательное число в меньшую сторону округление")
+
+
+
+# problem_9
+a = 1994
+b = 2020
+c = (b - a) + 2
+print(c,"Через 2 года")
+d = (b - a) - 2
+print(d,"2 года назад") 
+
+# problem_8
+a = 2021
+b = 79
+c = (a / (b - 100)) * 100
+print(c) 
+
+# problem_8
+a = (25 + 75 + 10 + 95) / 4
+print(a)
